0318b3fab5e27f0c56234d43195fb6949782d37b
keisukemurota <mukeisuke0709@gmail.com>
2021-12-30 16:14:26 +0900

    push

--- cmake/eigen3.cmake
+++ cmake/eigen3.cmake
@@ -1,15 +1,9 @@
 find_package(Eigen3)
-<<<<<<< HEAD
 if(Eigen3_FOUND)
 message("found")
 message("EIGEN3_USE_FILE  =  ${EIGEN3_USE_FILE}")
 else(Eigen3_FOUND)
 message("not found")
-=======
-message("EIGEN3_USE_FILE = ${EIGEN3_USE_FILE}")
-message("CMAKE_CURRENT_LIST_DIR = ${CMAKE_CURRENT_LIST_DIR}")
-
->>>>>>> 71444c3ef8d291752919e16b10ae69df7272d5da
   include(FetchContent)
   FetchContent_Declare(
     eigen3
@@ -18,7 +12,4 @@ message("CMAKE_CURRENT_LIST_DIR = ${CMAKE_CURRENT_LIST_DIR}")
   )
   list(APPEND FetchContent_includes "${PROJECT_BINARY_DIR}/_deps/eigen3-src")
   list(APPEND FetchContents eigen3)
-if(Eigen3_FOUND)
-  include(${EIGEN3_USE_FILE})
-else(Eigen3_FOUND)
 endif(Eigen3_FOUND)
