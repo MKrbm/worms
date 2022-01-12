9508589b904b9a38662463577335a020fabaa2db
keisukemurota <mukeisuke0709@gmail.com>
2022-01-12 21:08:31 +0900

    load .npy file

--- test_/npy_test.cpp
+++ test_/npy_test.cpp
@@ -2,50 +2,33 @@
 #include <string>
 #include <vector>
 #include <ndimvec.hpp>
-#include "npy.hpp"
+#include <load_npy.hpp>
+// #include <Shastry.hpp>
 
 using namespace std;
 
-int test_load(void) {
-  vector<unsigned long> shape;
-  bool fortran_order;
-  vector<double> data;
-
-  for (auto path : {"../python/array/H.npy"}) {
-    shape.clear();
-    data.clear();
-    npy::LoadArrayFromNumpy(path, shape, fortran_order, data);
-
-    cout << "shape: ";
-    for (size_t i = 0; i<shape.size(); i++)
-      cout << shape[i] << ", ";
-    cout << endl;
-    cout << "fortran order: " << (fortran_order ? "+" : "-");
-    cout << endl;
-    cout << "data: ";
-    for (size_t i = 0; i<data.size(); i++)
-      cout << data[i] << ", ";
-    cout << endl << endl;
-  }
-
-  return 0;
-}
-
-int test_save(void) {
-  const long unsigned leshape [] = {2,3};
-  vector<double> data {1, 2, 3, 4, 5, 6};
-  npy::SaveArrayAsNumpy("../python/array/out.npy", false, 2, leshape, data);
-
-  const long unsigned leshape2 [] = {6};
-  npy::SaveArrayAsNumpy("data/out2.npy", false, 1, leshape2, data);
-
-  return 0;
-}
 
 int main() {
-  test_load();
+  // auto ss = model::Shastry_2(2, 2, 1, 0);
 
-  // test_save();
+  auto pair = load_npy("../python/array/H.npy");
+
+  auto shape = pair.first;
+  auto data = pair.second;
+
+  printf("shape : [%d, %d]  \n", shape[0], shape[1]);
+
+  printf("data : \n");
+
+  for (int i=0; i<shape[0]; i++){
+    std::cout << std::endl << "[ ";
+    for (int j=0; j<shape[1]; j++)
+    {
+      // std::cout << data[i * shape[1] + j] << " ";
+      printf("%2.1f ", data[i * shape[1] + j]);
+    }
+    std::cout << "]" << std::endl;
+  }
 
   return 0;
 }
--- test_/test.cpp
+++ test_/test.cpp
@@ -1,21 +1,6 @@
-#include <uftree.hpp>
-#include <model.hpp>
-#include <worm.hpp>
-#include <observable.hpp>
-#include <iostream>
-#include <fstream>
-#include <stdio.h>
-#include <state.hpp>
 #include <chrono>
-#include <memory>
-#include <iterator>
-#include <cstdio>
-#include <tuple>
-#include <binstate.hpp>
-#include <lattice/graph.hpp>
-#include <lattice/coloring.hpp>
 #include <Shastry.hpp>
-#include <npy.hpp>
+// #include "npy.hpp"
 
 using namespace std::chrono;
 
@@ -29,25 +14,18 @@ using std::chrono::duration_cast;
 using std::chrono::duration;
 using std::chrono::milliseconds;
 using std::chrono::microseconds;
-int add(int a, int b){
-  return a+b;
-}
-
-inline int modifyBit(int n, int p, int b)
-{
-    return ((n & ~(1 << p)) | (b << p));
-}
-
-inline int getbit(int n, int p)
-{
-    return (n >> p) & 1;
-}
 
 int main(){
 
 
   auto ss = model::Shastry_2(2, 2, 1, 0);
-  ss.lattice.print(std::cout);
+  // ss.lattice.print(std::cout);
+
+  // std::cout << "bonds : \n" ;
+  // size_t ind;
+  // for (auto x : ss.bonds){
+  //   printf("[ %d, %d ] / type : %d\n", x[0], x[1], ss.bond_type[ind]);
+  //   ind++;
 
   // std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
 
