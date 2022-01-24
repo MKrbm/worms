7a96fe274ca77862a3355c337f56637117b20046
keisukemurota <mukeisuke0709@gmail.com>
2022-01-24 21:09:19 +0900

    add some python file

---
+++ python/check_conn_from_index.py
@@ -0,0 +1,95 @@
+from netrc import netrc
+import numpy as np
+import numpy as np
+from scipy import sparse
+
+
+def num2state(s, L = 4):
+    state = []
+    for i in range(L):
+        state.append(s%4)
+        s >>= 2
+    return state
+
+def state_update(index, leg, fl):
+    d = leg // 2
+    l = leg % 2
+    index[d] = index[d] ^ (fl << (l * 2))
+    return index
+
+J1 = 0.4
+J2 = 1
+
+
+h1 = np.load("array/SS_bond1.npy").round(4)
+h2 = np.load("array/SS_bond2.npy").round(4)
+ho = np.load("array/SS_onsite.npy").round(4)
+H = sparse.csr_matrix(J1*h1 + J2*ho) #this is the hamiltonian you wanna check the connectivity.
+shift = -min(np.diagonal(H.toarray()).min(), 0)
+
+
+H_tmp = H + shift*sparse.identity(H.shape[0])
+print(H_tmp)
+
+print("\n\n", "-"*20, "\n\n")
+
+
+
+indices = (np.array((H_tmp).nonzero()).T).tolist()
+index = [0, 0]
+index_ori = index.copy()
+assert index in indices, "index is not listed in non-zero index for the hamiltonian"
+
+
+indices_list = []
+for i in range(4):
+    for j in range(3):
+        for i1 in range(4):
+            for j1 in range(3):
+                tmp = state_update(index.copy(), i, j+1)
+                tmp = state_update(tmp, i1, j1+1)
+                if (tmp in indices and tmp not in indices_list):
+                    indices_list.append(tmp)
+                    print(tmp, end = " ")
+print("\n\n\n")
+indices_list = [index]
+cnt_list = [0]
+index_rem = indices.copy()
+index_bag = []
+index_rem.remove(index)
+
+mv_cnt = np.infty
+
+prev_dict = {str(index) : index}
+
+while indices_list:
+    index = indices_list.pop(0)
+    cnt = cnt_list.pop(0)
+    index_bag.append(index.copy())
+    for i in range(4):
+        for j in range(3):
+            for i1 in range(4):
+                for j1 in range(3):
+                    tmp = state_update(index.copy(), i, j+1)
+                    tmp = state_update(tmp, i1, j1+1)
+                    if (tmp in indices) and (tmp not in index_bag) and (tmp not in indices_list):
+                        indices_list.append(tmp.copy())
+                        cnt_list.append(cnt+1)
+                        if (tmp == [4,4]):
+                            print(index)
+                        prev_dict[str(tmp)] = index.copy()
+                        if H[tmp[0], tmp[1]]  < 0 and tmp[0] != tmp[1]:
+                            if mv_cnt > cnt+1:
+                                mv_cnt = cnt+1
+                                neg_index = tmp
+
+print("# of shortest steps need to reach first negative element is : ", mv_cnt, "\n where index is : ", neg_index)  
+
+index_ = neg_index
+while True:
+    prev_index = prev_dict[str(index_)]
+    if (index_==prev_index):
+        break
+    print(index_, "<- ", end="")
+    index_ = prev_index
+print(index_ori)
---
+++ python/shastry_exact.py
@@ -0,0 +1,54 @@
+import numpy as np
+from scipy import sparse
+
+from quspin.basis import spin_basis_1d, spin_basis_general
+from quspin.operators import hamiltonian,quantum_operator
+from quspin.tools.lanczos import lanczos_full,lanczos_iter,FTLM_static_iteration,LTLM_static_iteration
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+
+
+'''
+calculate exact 4 x 4 shastry model
+'''
+
+
+Lx, Ly = 4, 4 # linear dimension of spin 1 2d lattice
+N_2d = Lx*Ly # number of sites for spin 1
+s = np.arange(N_2d) # sites [0,1,2,....]
+x = s%Lx # x positions for sites
+y = s//Lx # y positions for sites
+T_x = (x+1)%Lx + Lx*y # translation along x-direction
+T_y = x +Lx*((y+1)%Ly) # translation along y-direction
+P_x = x + Lx*(Ly-y-1) # reflection about x-axis
+P_y = (Lx-x-1) + Lx*y # reflection about y-axis
+Z   = -(s+1) # spin inversion
+# basis_2d = spin_basis_general(N_2d,pauli=False)
+spin_basis_general(N_2d,kxblock=(T_x,0),kyblock=(T_y,0),pxblock=(P_x,0),pyblock=(P_y,0),zblock=(Z,0), pauli=True)
+
+# set up coupling 
+J1 = 0.4
+J2 = 1
+J_zz = [[J1,i,T_x[i]] for i in range(N_2d)]+[[J1,i,T_y[i]] for i in range(N_2d)] 
+J_zz = J_zz + [[J2,0,5],[J2,2,7],[J2,8,13],[J2,10,15],[J2,1,14],[J2,3,12],[J2,6,9],[J2,4,11]]
+J_xy = [[J1/2.0,i,T_x[i]] for i in range(N_2d)]+[[J1/2.0,i,T_y[i]] for i in range(N_2d)]
+J_xy = J_xy + [[J2/2.0,0,5],[J2/2.0,2,7],[J2/2.0,8,13],[J2/2.0,10,15],[J2/2.0,1,14],[J2/2.0,3,12],[J2/2.0,6,9],[J2/2.0,4,11]]
+ops_dict = dict(Jpm=[["+-",J_xy]],Jmp=[["-+",J_xy]],Jzz=[["zz",J_zz]])
+
+
+E = []
+for i in range(Lx*Ly+1):
+    print(i)
+    basis_2d = spin_basis_general(N_2d, Nup = i ,pauli=False)
+    H_ = quantum_operator(ops_dict,basis=basis_2d,dtype=np.float64, check_symm=False)
+    E_,V_ = H_.eigh({})
+    E.append(E_)
+E = np.concatenate(E,axis=0)
+
+try:
+  print("save complete")
+  np.save(f"../doc/data/shastry_exact_J1={J1}.npy", E)
+except:
+  print("specified folder not exsit")
+  np.save(f"shastry_exact_J1={J1}.npy", E)
