8f68e3cf45aa83e4084c3e23760a2216fbc7be58
keisukemurota <mukeisuke0709@gmail.com>
2022-06-23 03:14:51 +0000

    forked npy.cmake

--- cmake/npy.cmake
+++ cmake/npy.cmake
@@ -1,7 +1,7 @@
 include(FetchContent)
 FetchContent_Declare(
   npy
-  GIT_REPOSITORY https://github.com/llohse/libnpy.git
+  GIT_REPOSITORY https://github.com/MKrbm/libnpy.git
 )
 list(APPEND FetchContent_includes "${PROJECT_BINARY_DIR}/_deps/npy-src/include")
 list(APPEND FetchContents npy)
