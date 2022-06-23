126a49e4119ed7081af32b8f2e27b8cacc54489d
keisukemurota <mukeisuke0709@gmail.com>
2022-06-23 03:14:41 +0000

    deleted debuggin message

--- cmake/eigen3.cmake
+++ cmake/eigen3.cmake
@@ -1,19 +1,15 @@
-message(STATUS "find eigen3 : ${CMAKE_Fortran_COMPILER} is available")
-find_package(Eigen3 MODULE)
-message(STATUS "find eigen3 : ${CMAKE_Fortran_COMPILER} is available")
+find_package(Eigen3)
 if(Eigen3_FOUND)
-message("found")
-message("EIGEN3_USE_FILE  =  ${EIGEN3_USE_FILE}")
+message(STATUS "eigen3 was found path = ${eigen3_SOURCE_DIR}")
 else(Eigen3_FOUND)
-message("not found")
   include(FetchContent)
   FetchContent_Declare(
     eigen3
     URL https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.tar.bz2
     SOURCE_SUBDIR none
   )
-  
+
   list(APPEND FetchContent_includes "${PROJECT_BINARY_DIR}/_deps/eigen3-src")
   list(APPEND FetchContents eigen3)
-  message(STATUS "find eigen3  / fetch content: ${CMAKE_Fortran_COMPILER} is available")
+  message(STATUS "find eigen3  / fetch content")
 endif(Eigen3_FOUND)
