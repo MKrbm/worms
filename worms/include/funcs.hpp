8ff9715df9cdf08a947177ce285989eb123f127d
keisukemurota <mukeisuke0709@gmail.com>
2022-08-25 19:15:39 +0000

    add print and cout function for debug mode

---
+++ worms/include/funcs.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#ifndef NDEBUG
+#define printd(fmt, ...) printf(fmt, ##__VA_ARGS__)
+#define dout cout 
+#define ASSERT(condition, message) \
+    do { \
+        if (! (condition)) { \
+            std::cerr << "Assertion `" #condition "` failed in " << __FILE__ \
+                      << " line " << __LINE__ << ": " << message << std::endl; \
+            std::terminate(); \
+        } \
+    } while (false)
+#else
+#define ASSERT(condition, message) do { } while (false)
+#define printd(fmt, ...) 0
+#define dout 0 && cout
+#endif
+
+
+template<class T> std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec) {
+    os << "[ ";
+    for ( const T& item : vec )
+        os << item << ", ";
+    os << "]"; return os;
+}
