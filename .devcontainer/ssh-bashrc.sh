252b019ddc88dd87d68d839d44b880a61aee6879
keisukemurota <mukeisuke0709@gmail.com>
2023-01-19 00:39:22 +0900

    docker updated

---
+++ .devcontainer/ssh-bashrc.sh
@@ -0,0 +1,8 @@
+. ~/ssh-find-agent/ssh-find-agent.sh # for bash 
+ssh_find_agent -a
+if [ -z "$SSH_AUTH_SOCK" ]
+then
+   eval $(ssh-agent) > /dev/null
+   ssh-add -l >/dev/null || alias ssh='ssh-add -l >/dev/null || ssh-add && unalias ssh; ssh'
+fi
+
