d2a9902018f9db723a6b2044ae0e2bffd5608350
MKrbm <mukeisuke0709@gmail.com>
2023-01-19 16:13:14 +0900

    test2

--- test/header2.h
+++ test/header2.h
@@ -1,9 +1,6 @@
-#include <iostrem>
-#include <math.h>
-#include <boost/property_tree/ptree.hpp>
-
-
-// void print_int(int x){
-//   std::cout << x << std::endl;
-// }
+#include <iostream>
+namespace boost { namespace property_tree {
+    class ptree;
+  }
+}
 
---
+++ test/header_.h
@@ -0,0 +1,9 @@
+#include <iostrem>
+#include <math.h>
+#include <boost/property_tree/ptree.hpp>
+
+
+// void print_int(int x){
+//   std::cout << x << std::endl;
+// }
+
