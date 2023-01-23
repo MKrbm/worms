3932a80a97adcb4cac6b4643d8dfa2f671f4fe98
MKrbm <mukeisuke0709@gmail.com>
2023-01-23 13:56:30 +0900

    added conditional branch for when obs_path is empty string.

--- worms/include/autoobservable.hpp
+++ worms/include/autoobservable.hpp
@@ -50,36 +50,43 @@ class model::observable
 
       // * load path list
       std::vector<std::string> path_list;
-      get_npy_path(obs_path, path_list);
-
-      if (N_bond_t != path_list.size()){
-        std::cerr << "number of types observable does not match to number of local operators (or # of bond types)" << std::endl;
-        exit(1);
-      }
-
-      std::sort(path_list.begin(), path_list.end());
-
-      for (int p_i=0; p_i<path_list.size(); p_i++) {
-        model::local_operator<MCT> loperator = loperators[p_i];
-        std::string path = path_list[p_i];
-        auto pair = load_npy(path);
-        VS shape = pair.first;
-        VD data = pair.second;
-        size_t S = shape[0];
-        if (shape[0]!= shape[1]){ std::cerr << "require square matrix" << std::endl; exit(1); }
-        if (data.size() != loperator.ham_prime.size() * loperator.ham_prime.size()){ std::cerr << "size of observable does not match to size of local operator" << std::endl; exit(1); }
-        if (print) std::cout << "obs operator is read from " << path << std::endl;
-        std::vector<double> _obs(S*S);
-        for (int i=0; i<S; i++) for (int j=0; j<S; j++)
-        {
-          double y = data[i * S + j];
-          double x = loperator.ham_vector(i * S + j); // x is absolute of the $\hat{h}_\prime[j][i]$ (note that $\hat{h}_\prime$ is a local hamiltonian whose origin shifts by ene_shift from original local hamiltonian)
-          if (abs(y) < 1e-8) {_obs[i * S + j] = 0; continue;}
-          if (abs(x) < 1e-8) x = 0;
-          if (x == 0) { std::cerr << "denominator is zero for " << i << " " << j << "which lead to infinite variance"; exit(1); }
-          _obs[i * S + j] = y / x;
+      if (obs_path != ""){
+        get_npy_path(obs_path, path_list);
+        if (N_bond_t != path_list.size()){
+          std::cerr << "number of types observable does not match to number of local operators (or # of bond types)" << std::endl;
+          exit(1);
         }
-        _obs_operators.push_back(_obs);
+
+        std::sort(path_list.begin(), path_list.end());
+
+        for (int p_i=0; p_i<N_bond_t; p_i++) {
+          model::local_operator<MCT> loperator = loperators[p_i];
+          std::string path = path_list[p_i];
+          auto pair = load_npy(path);
+          VS shape = pair.first;
+          VD data = pair.second;
+          size_t S = shape[0];
+          if (shape[0]!= shape[1]){ std::cerr << "require square matrix" << std::endl; exit(1); }
+          if (data.size() != loperator.ham_prime.size() * loperator.ham_prime.size()){ std::cerr << "size of observable does not match to size of local operator" << std::endl; exit(1); }
+          if (print) std::cout << "obs operator is read from " << path << std::endl;
+          std::vector<double> _obs(S*S);
+          for (int i=0; i<S; i++) for (int j=0; j<S; j++)
+          {
+            double y = data[i * S + j];
+            double x = loperator.ham_vector(i * S + j); // x is absolute of the $\hat{h}_\prime[j][i]$ (note that $\hat{h}_\prime$ is a local hamiltonian whose origin shifts by ene_shift from original local hamiltonian)
+            if (abs(y) < 1e-8) {_obs[i * S + j] = 0; continue;}
+            if (abs(x) < 1e-8) x = 0;
+            if (x == 0) { std::cerr << "denominator is zero for " << i << " " << j << "which lead to infinite variance"; exit(1); }
+            _obs[i * S + j] = y / x;
+          }
+          _obs_operators.push_back(_obs);
+        }
+      } else {
+        for (int p_i=0; p_i<N_bond_t; p_i++) {
+          model::local_operator<MCT> loperator = loperators[p_i];
+          std::vector<double> _obs(loperator.ham_prime.size() * loperator.ham_prime.size(), 0);
+          _obs_operators.push_back(_obs);
+        }
       }
     }
 
