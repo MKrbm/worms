a388a43d1bd5dbe4f3d1ace08f3bd83e3dba499e
MKrbm <mukeisuke0709@gmail.com>
2023-02-10 13:32:08 +0900

    add test dataset

--- gtest/dataset.hpp
+++ gtest/dataset.hpp
@@ -13,3 +13,9 @@ static const std::vector<std::vector<double>> heisenberg1D_ham = { { -0.25, 0, 0
 
 static const std::vector<double> worm_obs_check = { 152.8 ,  83.42, 253.33,   7.95,  44.92,  81.57,  70.95, 176.72,
           126.09,  84.88,  68.77, 151.74, 204.77,  91.7 , 155.45, 133.93};
+
+
+// Jz_-1_Jx_-0.5_Jy_-0.3_hz_0_hx_0.5
+static const std::vector<double> check_warp_ham = {0.75,0.125,0.125,0.05,0.125,0.25,0.2,0.125,0.125,0.2,0.25,0.125,0.05,0.125,0.125, 0.75};
+
+static const std::vector<bool> check_has_warp = {true,true,true,false,true,false,false,true,true,false,false,true,false,true,true,true};
