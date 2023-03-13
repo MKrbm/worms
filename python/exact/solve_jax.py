bba7c04e17d8ecd97b31ac54a1e59b1cf4788f1a
MKrbm <mukeisuke0709@gmail.com>
2023-03-13 19:04:55 +0900

    modify jax solver

--- python/exact/solve_jax.py
+++ python/exact/solve_jax.py
@@ -1,7 +1,7 @@
 from jax_exact import KH
 import numpy as np
 import jax
-# from jax import numpy as jnp
+import os
 import argparse
 
 models = [
@@ -21,6 +21,7 @@ parser.add_argument('-L2', "--length2", help = "length of side", type = int)
 # parser.add_argument('-N','--n_samples', help='# of samples', type = int, default = 50)
 # parser.add_argument('-m','--n_Krylov', help='dimernsion of Krylov space', type = int, default = 50)
 if __name__ == '__main__':
+
     args = parser.parse_args()
     L1 = args.length1
     L2 = args.length2 if args.length2 is not None else L1
@@ -30,7 +31,16 @@ if __name__ == '__main__':
         Jz = args.coupling_z,
         h = args.magnetic,
     )
+    a = ""
+    for k, v in p.items():
+        v = float(v)
+        a += f"{k}_{v:.4g}_"
+    params_str = a[:-1]
     if (args.model == "KH"):
+        model_name = "KH" + f"_{L1}x{L2}"
+        path_name = f"out/{model_name}/{params_str}"
+        
+
         N = L1 * L2 * 3
         H = KH([L1, L2], p)
         print("defined Hamiltonian")
@@ -43,23 +53,58 @@ if __name__ == '__main__':
         # calculate the energy
         # T = jax.numpy.arange(0.1, 1, 0.1).reshape(1,-1)
 
-        beta = jax.numpy.linspace(0, 100, 1000).reshape(1,-1)
+        beta = jax.numpy.linspace(0, 10, 1001).reshape(1,-1)
         B = jax.numpy.exp(-beta*E[:,None])
         Z = B.sum(axis=0)
         E_mean = (E[:,None]*B).sum(axis=0) / Z
         E_square_mean = ((E*E)[:,None]*B).sum(axis=0) / Z
         beta = beta.reshape(-1)
         C = (E_square_mean - E_mean**2)*(beta**2)
+
+
+
+        # * save calculated data
+        file = f'{path_name}/eigenvalues.npy'
+        os.makedirs(os.path.dirname(file), exist_ok=True)
+        np.save(file, E)
+
+        file = f'{path_name}/eigenvalues.csv'
+        os.makedirs(os.path.dirname(file), exist_ok=True)
+        with open(file, 'w') as dat_file:  
+            dat_file.write("index, value\n")
+            for i, e in enumerate(E):
+                dat_file.write(f"{i}, {e:.60g}\n")
+
+        file = f'{path_name}/groundstate.npy'
+        os.makedirs(os.path.dirname(file), exist_ok=True)
+        np.save(file, V[:,0])
+
+        file = f'{path_name}/groundstate.csv'
+        os.makedirs(os.path.dirname(file), exist_ok=True)
+        with open(file, 'w') as dat_file:  
+            dat_file.write("index, value\n")
+            for i, v in enumerate(V[:,0]):
+                dat_file.write(f"{i}, {v:.60g}\n")
+        
+
+        file = f'{path_name}/statistics.csv'
+        os.makedirs(os.path.dirname(file), exist_ok=True)
+        with open(file, 'w') as dat_file:
+            dat_file.write("beta, energy_per_site, specific_heat\n")
+            for b, e, c, in zip(beta, E_mean, C):
+                dat_file.write(f"{b}, {e/N}, {c/N}\n")
+    
+
         # for b, e, c, in zip(beta, E_mean, (E_square_mean - E_mean**2)*(beta**2)):
-        for i in range(len(beta)):
-            b = beta[-i-1]
-            if b == 0:
-                b = np.inf
-            else :
-                b = 1/b
-            e = E_mean[-i-1]
-            c = C[-i-1]
-            print(f"{b} : {e} {c} {e / N} {c / N}")
+        # for i in range(len(beta)):
+        #     b = beta[-i-1]
+        #     if b == 0:
+        #         b = np.inf
+        #     else :
+        #         b = 1/b
+        #     e = E_mean[-i-1]
+        #     c = C[-i-1]
+        #     print(f"{b} : {e} {c} {e / N} {c / N}")
 
         # print(f"beta            = {beta}")
         # print(f"E               = {E_mean}")
