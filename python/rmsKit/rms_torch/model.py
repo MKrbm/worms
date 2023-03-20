a49fcd2a1572e83d4a812d26c17ac6e13499ed07
MKrbm <mukeisuke0709@gmail.com>
2023-03-20 23:15:15 +0900

    add reset_params method

--- python/rmsKit/rms_torch/model.py
+++ python/rmsKit/rms_torch/model.py
@@ -1,6 +1,6 @@
 import torch
-import numpy as np
 from torch import nn
+import numpy as np
 import math
 
 
@@ -9,20 +9,37 @@ def random_unitary_matrix(size: int, device: torch.device) -> torch.Tensor:
     q, _ = np.linalg.qr(random_matrix)
     return torch.tensor(q, dtype=torch.float64, device=device)
 
-class CustomModel(nn.Module):
-    def __init__(self, H_size: int, unitary_size: int, device = torch.device("cpu"), u0 = None):
-        super(CustomModel, self).__init__()
+
+
+class UnitaryRieman(nn.Module):
+    def __init__(self, H_size: int, unitary_size: int, device=torch.device("cpu"), u0=None):
+        super(UnitaryRieman, self).__init__()
 
         if H_size <= 0 or unitary_size <= 0:
             raise ValueError("Both H_size and unitary_size should be positive integers.")
 
-        n_us = round(math.log2(H_size) / math.log2(unitary_size))
-        if u0 is None:
-            self.us = nn.ParameterList([nn.Parameter(random_unitary_matrix(unitary_size, device), requires_grad=True) for _ in range(n_us)])
+        self.H_size = H_size
+        self.unitary_size = unitary_size
+        self.device = device
+        self.u0 = u0
+
+        self.initialize_params()
+
+    def initialize_params(self):
+        n_us = round(math.log2(self.H_size) / math.log2(self.unitary_size))
+        if self.u0 is None:
+            self.us = nn.ParameterList([nn.Parameter(random_unitary_matrix(self.unitary_size, self.device), requires_grad=True) for _ in range(n_us)])
         else:
             self.us = nn.ParameterList([
-                nn.Parameter(torch.tensor(u0, dtype = torch.float64, device = device), requires_grad=True)
+                nn.Parameter(torch.tensor(self.u0, dtype=torch.float64, device=self.device), requires_grad=True)
                 for _ in range(n_us)])
+
+    def reset_params(self):
+        for p in self.parameters():
+            if p.grad is not None:
+                p.grad.detach_()
+                p.grad.zero_()
+            p.data = random_unitary_matrix(self.unitary_size, self.device)
 
     def forward(self) -> torch.Tensor:
         U = self.us[0]
