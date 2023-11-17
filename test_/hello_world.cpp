6ff0d4ff1a9fc6de42a22deba1df7a8f849e55a0
keisukemurota <mukeisuke0709@gmail.com>
2023-11-17 14:50:28 +0100

    add mpitest

--- test_/hello_world.cpp
+++ test_/hello_world.cpp
@@ -1,25 +1,29 @@
 #include <mpi.h>
-// #include <iil.h>
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char** argv) {
-  MPI::Init(argc, argv);
+    // Initialize the MPI environment
+    MPI_Init(&argc, &argv);
 
-  // Get the number of processes
-  int world_size = MPI::COMM_WORLD.Get_size();
+    // Get the number of processes
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-  // Get the rank of the process
-  int world_rank = MPI::COMM_WORLD.Get_rank();
+    // Get the rank of the process
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
-  // Get the name of the processor
-  char processor_name[MPI_MAX_PROCESSOR_NAME];
-  int name_len;
-  MPI::Get_processor_name(processor_name,name_len);
+    // Get the name of the processor
+    char processor_name[MPI_MAX_PROCESSOR_NAME];
+    int name_len;
+    MPI_Get_processor_name(processor_name, &name_len);
 
-  std::cout << "Hello World from processor " << processor_name << ", rank "
-            << world_rank << " out of " << world_size << " processors\n";
+    printf("Hello World from processor %s, rank %d out of %d processors\n",
+           processor_name, world_rank, world_size);
 
-  // Finalize the MPI environment.
-  MPI::Finalize();
-  std::cout << "Hello World from processor " << ", rank " << " out of " << " processors" << std::endl;
+    // Finalize the MPI environment.
+    MPI_Finalize();
+
+    return 0;
 }
