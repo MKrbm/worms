4f769478f1ce0c0cb4b670d34e8262a8c5445665
Keisuke Murota <keisuke@zetta-01.phys.s.u-tokyo.ac.jp>
2023-12-08 10:35:06 +0900

    modify bash script

--- job/hxyz1d_optim.sh
+++ job/hxyz1d_optim.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
 
 # Function to run a job with specific values of J and H
+
+
 run_job() {
-	PROJECT_DIR=/Users/keisukemurota/Documents/todo/worms
+  # Get the directory of the current script
+  PROJECT_DIR=$(dirname "$(pwd)")
   local J=$1
   local H=$2
   cd $PROJECT_DIR/python/rmsKit || return
+  source ~/worms/myenv/bin/activate
   export OMP_NUM_THREADS=1
   export MKL_NUM_THREADS=1
   echo "Running job with J=$J and H=$H"
   python -u new_optimize_loc.py \
-    -m HXYZ1D -loss mel -o Adam --lattice_type 1  -M 10 -e 100 -lr 0.005 \
+    -m HXYZ1D -loss mel -o Adam --lattice_type 2  -M 40 -e 2000 -lr 0.005 \
     -Jz 1.0 -Jx $J -Jy $J -hx $H \
-    --stdout > $PROJECT_DIR/job/optimize/J_${J}_H_${H}_output.log
+    --stdout >> $PROJECT_DIR/job/optimize/J_${J}_H_${H}_output.log
+  deactivate	
 }
 
 export -f run_job
 
-NUM_CPU=6
+NUM_CPU=90
 
 # Generate the list of J and H values
 generate_values() {
-	for H in $(seq -10 0.5 10); do
-		for J in $(seq -10 0.5 10); do
+  for J in $(seq -10 0.5 10); do
+    for H in $(seq -10 0.5 10); do
       echo $J $H
     done
   done
