b6382044d00b38174c4b4f57c7253882290344e3
keisukemurota <mukeisuke0709@gmail.com>
2023-12-11 22:47:18 +0100

    fixed cmake bug

---
+++ cmake/lattice.cmake
@@ -0,0 +1,12 @@
+option(LATTICE_INSTALL OFF)
+option(LATTICE_BUILD_TESTS OFF)
+option(LATTICE_BUILD_EXAMPLES OFF)
+
+include(FetchContent)
+FetchContent_Declare(
+  lattice
+  GIT_REPOSITORY https://github.com/todo-group/lattice.git
+)
+set(lattice_dir "${PROJECT_BINARY_DIR}/_deps/lattice-src")
+list(APPEND FetchContent_includes "${PROJECT_BINARY_DIR}/_deps/lattice-src")
+list(APPEND FetchContents lattice)
