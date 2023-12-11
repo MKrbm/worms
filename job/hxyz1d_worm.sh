ddfb24dbb3b60a62804d4881e18967e32821e908
keisukemurota <mukeisuke0709@gmail.com>
2023-12-11 22:50:07 +0100

    update bash

--- job/hxyz1d_worm.sh
+++ job/hxyz1d_worm.sh
@@ -8,11 +8,13 @@ run_job() {
   local J=$1
   local H=$2
   cd $PROJECT_DIR/python/rmsKit || return
+  source ~/worms/myenv/bin/activate
   export OMP_NUM_THREADS=1
   export MKL_NUM_THREADS=1
   echo "Running job with J=$J and H=$H"
 	python -u -m run_worm  -m HXYZ1D -f $PROJECT_DIR/python/rmsKit/array/torch/HXYZ1D_loc/Jx_${J}_Jy_${J}_Jz_1_hx_${H}_hz_0_lt_1/ -s 10000\
-	>> $PROJECT_DIR/job/worm/J_${J}_H_${H}_output.log
+	>> $PROJECT_DIR/job/korm/J_${J}_H_${H}_output.log
+	deactivate
 }
 
 export -f run_job
