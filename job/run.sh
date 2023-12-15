89c05b60cf3bb13eb820d910ef546feee602e363
Keisuke Murota <keisuke@zetta-01.phys.s.u-tokyo.ac.jp>
2023-12-15 20:23:35 +0900

    modify bash

--- job/run.sh
+++ job/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Source the shared script
+#n: Source the shared script
 # source hxyz1d_sim.sh
 source blbq1d_sim.sh
 
 P=2
 PROJECT_DIR=$(dirname "$(pwd)")
-NUM_THREAD=2  # Adjust this to the number of parallel jobs you want to run
+NUM_THREAD=40  # Adjust this to the number of parallel jobs you want to run
 
 # Cleanup existing job data
 echo "removing $PROJECT_DIR/job/worm/*"
@@ -15,6 +15,6 @@ rm $PROJECT_DIR/job/worm/*
 echo "PROJECT_DIR: $PROJECT_DIR"
 #source /opt/materiapps-intel/env.sh
 #source ~/worms/myenv/bin/activate
-#export PATH=~/worms/myenv/bin:$PATH
+
 # Use GNU Parallel to run jobs in parallel
 seq 0 $(($total_jobs - 1)) | parallel -j $NUM_THREAD run_job {} $PROJECT_DIR $P
