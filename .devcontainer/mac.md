6c3e2a0e96543801afdcfe38df5f63b3f7345398
MKrbm <mukeisuke0709@gmail.com>
2024-05-13 05:04:32 +0000

    remove .devcontainer

--- .devcontainer/mac.md
+++
@@ -1,3 +0,0 @@
-- ### Error for torchvision for mac container.
-
-  - 
--- .devcontainer/ssh-bashrc.sh
+++
@@ -1,9 +0,0 @@
-# ssh-find-agent
-. ~/ssh-find-agent/ssh-find-agent.sh # for bash 
-ssh_find_agent -a
-if [ -z "$SSH_AUTH_SOCK" ]
-then
-   eval $(ssh-agent) > /dev/null
-   ssh-add -l >/dev/null || alias ssh='ssh-add -l >/dev/null || ssh-add && unalias ssh; ssh'
-fi
-
