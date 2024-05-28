28b995f4e1225e2dce1c08038504cebce729a962
MKrbm <mukeisuke0709@gmail.com>
2024-05-28 11:29:19 +0900

    removed phase

--- python/rmsKit/rms_torch/model.py
+++ python/rmsKit/rms_torch/model.py
@@ -30,7 +30,6 @@ class UnitaryRiemann(nn.Module):
         device: torch.device = torch.device("cpu"),
         u0: Optional[torch.Tensor] = None,
         dtype: torch.dtype = torch.float64,
-        only_phase: bool = False,
     ):
         """Initialize UnitaryRiemann class."""
         super(UnitaryRiemann, self).__init__()
@@ -51,9 +50,6 @@ class UnitaryRiemann(nn.Module):
                 raise ValueError("The dtype of u0 does not match the specified dtype.")
             if not check_is_unitary_torch(X=u0):
                 raise ValueError("u0 must be a unitary matrix.")
-        if only_phase and not is_complex(dtype):
-            logger.warning("dtype == float contains phase information. If you want to use only_phase, please set dtype == complex. only_phase will be set to False.")
-            only_phase = False
 
         self.H_size = H_size
         self.unitary_size = unitary_size
@@ -61,17 +57,13 @@ class UnitaryRiemann(nn.Module):
         self.u0 = u0
         self.dtype = dtype
         self.num_repeat = round(np.emath.logn(unitary_size, H_size))
-        self.only_phase = only_phase
 
         self.initialize_params()
 
     def initialize_params(self):
         """Initialize parameters of UnitaryRiemann class."""
         # n_us = round(math.log2(self.H_size) / math.log2(self.unitary_size))
-        if self.only_phase:
-            dtype = torch.float64 if self.dtype == torch.complex128 else torch.float32
-        else:
-            dtype = self.dtype
+        dtype = self.dtype
         if self.u0 is None:
             self.u = nn.ParameterList(
                 [
@@ -91,9 +83,6 @@ class UnitaryRiemann(nn.Module):
                 ]
             )
         
-        if self.only_phase:
-            self.phase = nn.Parameter(torch.tensor(np.random.randn(), dtype=torch.float64, device=self.device), requires_grad=True)
-
     def reset_params(self, u0: Union[torch.Tensor, None] = None):
         """Reset parameters of UnitaryRiemann class."""
         if u0 is not None and u0.shape != (self.unitary_size, self.unitary_size):
@@ -104,14 +93,11 @@ class UnitaryRiemann(nn.Module):
                 if u0 is None
                 else u0.clone().detach().to(self.dtype).to(self.device)
             )
-        if self.only_phase:
-            self.phase.data[:] = torch.tensor(np.random.randn(), dtype=self.dtype, device=self.device)
+
 
     def forward(self) -> torch.Tensor:
         """Calculate kron of unitary matrix (result size must be H_size x H_size)."""
         U = self.u[0]
-        if self.only_phase:
-            U = U * torch.exp(1j * self.phase)
         for i in range(self.num_repeat - 1):
             U = torch.kron(U, self.u[0])
         return U
@@ -120,27 +106,9 @@ class UnitaryRiemann(nn.Module):
         """
         Update the Riemannian gradient of the unitary matrix for each parameter.
         """
-        # for p in self.parameters():
-        #     if p.grad is not None:
-        #         if not self.is_params_phase(p):
-        #             p.grad[:] = riemannian_grad_torch(p.data, p.grad)
         for i in range(len(self.u)):
             self.u[i].grad[:] = riemannian_grad_torch(self.u[i], self.u[i].grad)
     
-    def update_phase(self, step: float):
-        """
-        Update the phase of the unitary matrix for each parameter.
-        """
-        if self.phase is None:
-            raise ValueError("The phase is not defined.")
-        if self.phase.grad is None:
-            raise ValueError("The phase.grad is not initialized.")
-        self.phase.data[:] = self.phase.data[:] - step * self.phase.grad[:]
-    
-    def is_params_phase(self, p : nn.Parameter):
-        return p.shape == torch.Size([])
-        
-
     
 
 
